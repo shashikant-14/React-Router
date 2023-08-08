f8522730a4040ee7c177c299807a6f00b375a35d
shashikant <pshashikant1998@gmail/com>
2023-08-08 16:54:30 +0530

    initial commit

---
+++ src/components/About.js
@@ -0,0 +1,7 @@
+const About = () => {
+    return(
+        <h1>This is About component</h1>
+    )
+}
+
+export default About;
---
+++ src/components/Home.js
@@ -0,0 +1,7 @@
+const Home = () => {
+    return(
+        <h1>This is Home Component</h1>
+    )
+}
+
+export default Home;
